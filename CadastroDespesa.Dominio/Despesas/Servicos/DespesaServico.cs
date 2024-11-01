bd5c3f8db39a611660e7022b59654ec90846c389
lajuniorfe <33765211+lajuniorfe@users.noreply.github.com>
2024-11-01 18:15:11 -0300

    Cria teste para entidade e serviço Despesa

--- CadastroDespesa.Dominio/Despesas/Servicos/DespesaServico.cs
+++ CadastroDespesa.Dominio/Despesas/Servicos/DespesaServico.cs
@@ -1,15 +1,10 @@
-using System;
 using CadastroDespesa.Dominio.Categorias.Entidades;
-using CadastroDespesa.Dominio.Categorias.Servicos;
 using CadastroDespesa.Dominio.Categorias.Servicos.Interfaces;
 using CadastroDespesa.Dominio.Despesas.Entidades;
 using CadastroDespesa.Dominio.Despesas.Repositorios;
 using CadastroDespesa.Dominio.Despesas.Servicos.Interfaces;
 using CadastroDespesa.Dominio.TipoDespesas.Entidades;
-using CadastroDespesa.Dominio.TipoDespesas.Servicos;
 using CadastroDespesa.Dominio.TipoDespesas.Servicos.Interfaces;
-using CadastroDespesa.Dominio.TiposPagamento.Entidades;
-using CadastroDespesa.Dominio.TiposPagamento.Servicos;
 using CadastroDespesa.Dominio.TiposPagamento.Servicos.Interfaces;
 
 namespace CadastroDespesa.Dominio.Despesas.Servicos;
@@ -19,28 +14,27 @@ public class DespesaServico : IDespesaServico
     private readonly IDespesaRepositorio despesasRepositorio;
     private readonly ICategoriaServico categoriaServico;
     private readonly ITipoDespesaServico tipoDespesaServico;
-    private readonly ITipoPagamentoServico tipoPagamentoServico;
 
 
-    public DespesaServico(IDespesaRepositorio despesasRepositorio, ICategoriaServico categoriaServico, ITipoDespesaServico tipoDespesaServico, ITipoPagamentoServico tipoPagamentoServico)
+    public DespesaServico(IDespesaRepositorio despesasRepositorio, ICategoriaServico categoriaServico, ITipoDespesaServico tipoDespesaServico)
     {
         this.despesasRepositorio = despesasRepositorio;
         this.categoriaServico = categoriaServico;
         this.tipoDespesaServico = tipoDespesaServico;
-        this.tipoPagamentoServico = tipoPagamentoServico;
     }
 
     public async Task<Despesa> ValidarDespesaAsync(int idDespesa){
         return await despesasRepositorio.ObterPorId(idDespesa);
     }
-    public async Task<Despesa> InstanciaDespesaParaCadastro(string descricao, decimal valor, int idCategoria, int idTipoDespesa, int idTipoPagamento)
+
+    public async Task<Despesa> InstanciaDespesaParaCadastro(string descricao, decimal valor, DateTime data,  int idCategoria, int idTipoDespesa)
     {
 
         Categoria categoria = await categoriaServico.ValidarCategoriaAsync(idCategoria);
 
         TipoDespesa tipoDespesa = await tipoDespesaServico.ValidarTipoDespesaAsync(idTipoDespesa);
 
-        Despesa despesa = new(descricao, valor, categoria, tipoDespesa);
+        Despesa despesa = new(descricao, valor, data, categoria, tipoDespesa);
 
         return despesa;
     }
