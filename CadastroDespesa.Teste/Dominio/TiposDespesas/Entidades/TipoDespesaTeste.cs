52fa76c46dab95fda0c7edaf87e15763ded827bb
lajuniorfe <33765211+lajuniorfe@users.noreply.github.com>
2024-11-03 19:11:31 -0300

    Cria testes entidade TipoDespesa

---
+++ CadastroDespesa.Teste/Dominio/TiposDespesas/Entidades/TipoDespesaTeste.cs
@@ -0,0 +1,25 @@
+﻿using CadastroDespesa.Dominio.TipoDespesas.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroDespesa.Teste.Dominio.TiposDespesas.Entidades
+{
+    public class TipoDespesaTeste
+    {
+        [Fact]
+        public void Quando_Instanciar_TipoDespesa_Espero_TipoDespesa_Validos()
+        {
+            string nome = "Tipo Despesa Teste";
+            string descricao = "Testar Tipo Despesa";
+
+            TipoDespesa tipoDespesa = new TipoDespesa(nome, descricao);
+
+            Assert.Equal(nome, tipoDespesa.Nome);
+            Assert.Equal(descricao, tipoDespesa.Descricao);
+        }
+
+    }
+}
