9299a4699ef6519ca3b96ff847eb84f1201a5e12
lajuniorfe <33765211+lajuniorfe@users.noreply.github.com>
2024-11-03 20:37:35 -0300

    Cria testes e ajustes entidade Fatura

--- CadastroDespesa.Dominio/Faturas/Servicos/Interfaces/IFaturaServico.cs
+++ CadastroDespesa.Dominio/Faturas/Servicos/Interfaces/IFaturaServico.cs
@@ -6,8 +6,8 @@ namespace CadastroDespesa.Dominio.Faturas.Servicos.Interfaces
     public interface IFaturaServico
     {
         Task<Fatura> ValidarFaturaAsync(int id);
-
-        Task<Fatura> VerificarFaturaCartaoAsync(Cartao cartao, decimal valorDespesa, DateTime dataFatura);
-
+        Task<Fatura> VerificarFaturaCartaoAsync(int idCartao, decimal valorDespesa, DateTime dataFatura);
+        Task<Fatura> AlterarFaturaCartaoExistenteAsync(Fatura faturaCartaoExistente, decimal valorDespesa);
+        Task<Fatura> CriarFaturaCartaoAsync(DateTime dataFatura, Cartao cartao, decimal valor);
     }
 }
---
+++ CadastroDespesa.Teste/Dominio/Faturas/Entidades/FaturaTeste.cs
@@ -0,0 +1,30 @@
+﻿using CadastroDespesa.Dominio.Cartoes.Entidades;
+using CadastroDespesa.Dominio.Faturas.Entidades;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroDespesa.Teste.Dominio.Faturas.Entidades
+{
+    public class FaturaTeste
+    {
+        [Fact]
+        public void Quando_Instancia_Fatura_Espero_Valores_Validos()
+        {
+            decimal valor = 10;
+            DateTime dataVencimento = DateTime.Now;
+            DateTime MesCorrespondente = DateTime.Now;
+            Cartao cartaoValidoMock = new Mock<Cartao>().Object;
+
+            Fatura fatura = new(valor, dataVencimento.Date, MesCorrespondente.Date, cartaoValidoMock);
+
+            Assert.Equal(valor, fatura.Valor);
+            Assert.Equal(dataVencimento.Date, fatura.DataVencimento);
+            Assert.Equal(MesCorrespondente.Date, fatura.MesCorrespondente.Date);
+            Assert.Equal(cartaoValidoMock, fatura.Cartao);
+        }
+    }
+}
